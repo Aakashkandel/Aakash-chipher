eaf29c01f94a97a4e31543bd20e0fc81b7970f51
Aakashkandel <aakashkandel9777@gmail.com>
2024-06-30 19:26:20 +0545

    manage code

--- index.js
+++ index.js
@@ -1,74 +1,58 @@
-
-let data = "hey lalit k cha khabar";
-data = data.toLowerCase();
-let secretkey = "evefdsafds";
-
-if (secretkey.length < 5) {
-    console.log("Secret must be more than 5 character");
-    return;
-
-}
-function masterkeycode(index) {
-    let limit = secretkey.length;
-    return secretkey.charCodeAt(index % limit);
-}
-let encodemessage = [];
-let decodemessage = []
-
-
-function encodefunction(code) {
-    if (code > 26) {
-        code = 97 + code - 26 - 1;
-
-    }
-    else {
-        code = 97 + code - 1;
-    }
-    encodemessage.push(String.fromCharCode(code));
-}
-
-//encodig code
-for (i = 0; i < data.length; i++) {
-    let keycode = masterkeycode(i);
-    let messagecode = data.charCodeAt(i);
-
-    let sumcode = (keycode - 97) + (messagecode - 97) + 2;
-
-    encodefunction(sumcode);
-
-}
-
-
-let joinencodemessage = encodemessage.join('')
-console.log(joinencodemessage);
-
-
-
-
-
-//decoding code
-
-function decodefunction(decodecode) {
-    decodemessage.push(String.fromCharCode(decodecode + 97 - 1))
-
-}
-
-
-for (i = 0; i < encodemessage.length; i++) {
-    let decodekey = masterkeycode(i);
-    let decodesecretkey = joinencodemessage.charCodeAt(i);
-
-    let subcode = (decodesecretkey - 97) - (decodekey - 97);
-    if (subcode < 0) {
-        subcode += 26;
+function encodeMessage(data, secretKey) {
+    data = data.toLowerCase();
+    if (secretKey.length < 5) {
+        throw new Error("Secret must be more than 5 characters");
     }
 
-    decodefunction(subcode)
+    let encodemessage = [];
 
+    function masterKeyCode(index) {
+        let limit = secretKey.length;
+        return secretKey.charCodeAt(index % limit);
+    }
+
+    function encodeFunction(code) {
+        if (code > 26) {
+            code = 97 + code - 26 - 1;
+        } else {
+            code = 97 + code - 1;
+        }
+        encodemessage.push(String.fromCharCode(code));
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        let keycode = masterKeyCode(i);
+        let messagecode = data.charCodeAt(i);
+        let sumcode = (keycode - 97) + (messagecode - 97) + 2;
+        encodeFunction(sumcode);
+    }
+
+    return encodemessage.join('');
 }
 
+function decodeMessage(encodedData, secretKey) {
+    let decodemessage = [];
 
-let normalmsg = decodemessage.join('');
-normalmsg = normalmsg.replace(/:/g, ' ');
-console.log(normalmsg);
+    function masterKeyCode(index) {
+        let limit = secretKey.length;
+        return secretKey.charCodeAt(index % limit);
+    }
 
+    function decodeFunction(decodecode) {
+        decodemessage.push(String.fromCharCode(decodecode + 97 - 1));
+    }
+
+    for (let i = 0; i < encodedData.length; i++) {
+        let decodekey = masterKeyCode(i);
+        let decodesecretkey = encodedData.charCodeAt(i);
+        let subcode = (decodesecretkey - 97) - (decodekey - 97);
+        if (subcode < 0) {
+            subcode += 26;
+        }
+        decodeFunction(subcode);
+    }
+
+    return decodemessage.join('').replace(/:/g, ' ');
+}
+
+module.exports = { encodeMessage, decodeMessage };
